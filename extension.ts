f02c118b670452faf4374a85fadb669d9222b01a
Yusheng <yshngg@outlook.com>
2026-02-08 21:39:11 +0800

    style: format extension.ts with ESLint rules
    - Apply consistent code formatting with ESLint
    - Use double quotes for string literals
    - Improve code readability and maintainability
    - Fix import formatting and spacing

--- extension.ts
+++ extension.ts
@@ -2,39 +2,50 @@ import GLib from "gi://GLib";
 import Gio from "gi://Gio";
 import Meta from "gi://Meta";
 import Shell from "gi://Shell";
-import GObject from 'gi://GObject';
+import GObject from "gi://GObject";
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
-import { Extension, gettext as _ } from "resource:///org/gnome/shell/extensions/extension.js";
-import { QuickToggle, SystemIndicator } from 'resource:///org/gnome/shell/ui/quickSettings.js';
+import {
+  Extension,
+  gettext as _,
+} from "resource:///org/gnome/shell/extensions/extension.js";
+import {
+  QuickToggle,
+  SystemIndicator,
+} from "resource:///org/gnome/shell/ui/quickSettings.js";
 
 const ExampleToggle = GObject.registerClass(
-class ExampleToggle extends QuickToggle {
+  class ExampleToggle extends QuickToggle {
     constructor() {
-        super({
-            title: _('Smile'),
-            iconName: 'face-smile-symbolic',
-            toggleMode: true,
-        });
+      super({
+        title: _("Smile"),
+        iconName: "face-smile-symbolic",
+        toggleMode: true,
+      });
     }
-});
+  },
+);
 
 const ExampleIndicator = GObject.registerClass(
-class ExampleIndicator extends SystemIndicator {
+  class ExampleIndicator extends SystemIndicator {
     _indicator: any;
 
     constructor() {
-        super();
+      super();
 
-        this._indicator = this._addIndicator();
-        this._indicator.iconName = 'face-smile-symbolic';
+      this._indicator = this._addIndicator();
+      this._indicator.iconName = "face-smile-symbolic";
 
-        const toggle = new ExampleToggle();
-        toggle.bind_property('checked',
-            this._indicator, 'visible',
-            GObject.BindingFlags.SYNC_CREATE);
-        this.quickSettingsItems.push(toggle);
+      const toggle = new ExampleToggle();
+      toggle.bind_property(
+        "checked",
+        this._indicator,
+        "visible",
+        GObject.BindingFlags.SYNC_CREATE,
+      );
+      this.quickSettingsItems.push(toggle);
     }
-});
+  },
+);
 
 export default class TypeScriptTemplateExtension extends Extension {
   gsettings?: Gio.Settings;
@@ -46,7 +57,9 @@ export default class TypeScriptTemplateExtension extends Extension {
     this.animationsEnabled = this.gsettings.get_boolean("animate") ?? true;
 
     this._indicator = new ExampleIndicator();
-    Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator as any);
+    Main.panel.statusArea.quickSettings.addExternalIndicator(
+      this._indicator as any,
+    );
   }
 
   disable() {
@@ -55,7 +68,7 @@ export default class TypeScriptTemplateExtension extends Extension {
       this._indicator.destroy();
       this._indicator = undefined;
     }
-    
+
     this.gsettings = undefined;
   }
 }
